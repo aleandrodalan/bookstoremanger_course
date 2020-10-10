4a635bcc7302279293239dbf95217c3b28a81fe3
Aleandro P. Dalan <aleandro.dalan@loglabdigital.com.br>
2020-10-10 18:40:53 -0400

    First commit

---
+++ src/main/java/com/aleandrodalan/bookstoremanage/BookstoremanageApplication.java
@@ -0,0 +1,13 @@
+package com.aleandrodalan.bookstoremanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanageApplication.class, args);
+	}
+
+}
